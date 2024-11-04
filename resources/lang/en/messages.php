1580980c93aba3a87287da0cd1ca2aceb9024b54
Arn27 <116111992+Arn27@users.noreply.github.com>
2024-11-04 01:50:21 +0000

    Prideta daugiau tekstu

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -1,6 +1,12 @@
 <?php
 
 return [
-    'welcome' => 'Sveiki atvykę į konferencijų sistemą',
-
+    'home' => 'Home',
+    'welcome' => 'Welcome',
+    'student_info' => 'Student Info',
+    'client_subsystem' => 'Client Subsystem',
+    'employee_subsystem' => 'Employee Subsystem',
+    'admin_subsystem' => 'Admin Subsystem',
+    'user' => 'User',
+    'logout' => 'Logout'
 ];
--- resources/lang/lt/messages.php
+++ resources/lang/lt/messages.php
@@ -1,6 +1,12 @@
 <?php
 
 return [
-    'welcome' => 'Sveiki atvykę į konferencijų sistemą',
-
+    'home' => 'Pagrindinis puslapis',
+    'welcome' => 'Sveiki atvykę',
+    'student_info' => 'Studento informacija',
+    'client_subsystem' => 'Kliento posistemis',
+    'employee_subsystem' => 'Darbuotojo posistemis',
+    'admin_subsystem' => 'Administratoriaus posistemis',
+    'user' => 'Naudotojas',
+    'logout' => 'Atsijungti'
 ];
